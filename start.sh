92c013f5966b33b987744d448e0f6280a75f6995
john1king <master@john1king.com>
2016-07-02 16:47:45 +0800

    支持启动多个节点

--- start.sh
+++ start.sh
@@ -1,10 +1,28 @@
 #! /usr/bin/env bash
 
+i=1
+while [ $i -lt $N ]
+do
+  sudo mkdir -p /hadoop/dfs/data$i
+  sudo docker rm -f hadoop-slave$i
+  echo "start hadoop-slave$i ..."
+  sudo docker run -d \
+    -v /hadoop/dfs/data$i:/hadoop/dfs/data \
+    --net=hadoop \
+    --name=hadoop-slave$i \
+    --hostname=hadoop-slave$i \
+    jk/datanode
+  i=$(( $i + 1 ))
+done
 
-docker rm $(docker ps -a -q)
-
-mkdir -p /hadoop/dfs/name
-mkdir -p /hadoop/dfs/data1
-
-docker run -d -v /hadoop/dfs/data1:/hadoop/dfs/data --net=hadoop --name=hadoop-slave1 --hostname=hadoop-slave1 jk/datanode
-docker run -d -v /hadoop/dfs/name:/hadoop/dfs/name -p 50070:50070 -p 8088:8088 --net=hadoop --name=hadoop-master --hostname=hadoop-master jk/namenode
+sudo mkdir -p /hadoop/dfs/name
+sudo docker rm -f hadoop-master
+echo "start hadoop-master ..."
+sudo docker run -d \
+  -v /hadoop/dfs/name:/hadoop/dfs/name \
+  -p 50070:50070 \
+  -p 8088:8088 \
+  --net=hadoop \
+  --name=hadoop-master \
+  --hostname=hadoop-master \
+  jk/namenode
