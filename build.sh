85b9a4e52f2161cd86e7392c0e086bdeaa8d3e4b
zhangyijin <zhangyijin@bilibili.com>
2016-07-03 15:38:43 +0800

    fix build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
 docker build -t jk/hadoop ./hadoop
-docker run -d --net=hadoop --hostname hadoop-slave1 --name hadoop-slave1 jk/datanode
+docker build -t jk/namenode ./namenode
 docker build -t jk/datanode ./datanode
 docker build -t jk/hive ./hive
